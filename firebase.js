7c537557f85ededcfd68300307847b252feb933d
Billboah <wyeboah20@sahs.uhas.edu.gh>
2022-07-03 16:13:19 +0000

    another commit

--- firebase.js
+++ firebase.js
@@ -2,14 +2,14 @@
 import { initializeApp } from 'firebase/app'
 import { GoogleAuthProvider, getAuth } from 'firebase/auth'
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: 'AIzaSyAnBccgHoWsp1ETSgkEGD3hXwF6s3zngdE',
-  authDomain: 'uber-next-clone-ce64a.firebaseapp.com',
-  projectId: 'uber-next-clone-ce64a',
-  storageBucket: 'uber-next-clone-ce64a.appspot.com',
-  messagingSenderId: '963353368442',
-  appId: '1:963353368442:web:4bc70b6aaaa5152c19a8c3',
+  apiKey: 'AIzaSyA3HqmIlsUGYzBCnMHsbeeh-rTu1JwAccQ',
+  authDomain: 'clone-e1d3e.firebaseapp.com',
+  projectId: 'clone-e1d3e',
+  storageBucket: 'clone-e1d3e.appspot.com',
+  messagingSenderId: '1093371412474',
+  appId: '1:1093371412474:web:707528face33f53b1cae43',
+  measurementId: 'G-JB4YBGR6ZZ',
 }
 
 // Initialize Firebase
